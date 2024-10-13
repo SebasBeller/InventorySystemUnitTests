4f1060e7a599ebb5c22f676460c1c52b700005b4
Wama85 <91913467+Wama85@users.noreply.github.com>
2024-10-12 20:56:10 -0400

    Realizar pruebas unitarias del modulo ticket (#11)
    * Se implementan pruebas unitarias en tabla compras
    
    * Se implementan pruebas unitarias en ticket
    
    * se implementan pruebas initarias para el modulo de G_compras ticket
    
    ---------
    
    Co-authored-by: susan-cespedes <susan.cespedes@ucb.edu.bo>
    Co-authored-by: SebasBeller <121450828+SebasBeller@users.noreply.github.com>

--- tests/G_compra/test_ticket.py
+++ tests/G_compra/test_ticket.py
@@ -2,8 +2,36 @@ import unittest
 from Modelo.G_compra import Ticket
 
 class TestTicket(unittest.TestCase):
+  
     def setUp(self):
         self.ticket = Ticket(1,"1/1/2024")
-    def test_agregar_ticket(self):
-        self.ticket.agregar_producto("modelo",1,50)
-        self.assertNotEqual(self.ticket.productos,[])
+        self.ticket.productos=[
+            {"modelo":"modeloA","cantidad":1,"precio":200},
+            {"modelo": "modeloB", "cantidad": 2, "precio": 10}
+        ]
+        
+    def tearDown(self):
+        self.ticket=None
+
+    def test_agregar_producto(self):
+        producto={"modelo":"modeloC","cantidad":1,"precio":50}
+        self.ticket.agregar_producto(**producto)
+        self.assertIn(producto,self.ticket.productos)
+
+    def test_obtener_total(self):
+        total=self.ticket.obtener_total()
+        self.assertEqual(total,220)
+
+    def test_obtener_total_vacio(self):
+        self.ticket.productos=[]
+        total=self.ticket.obtener_total()
+        self.assertEqual(total,0)
+
+    def test_obtener_productos(self):
+        productos=self.ticket.obtener_productos()
+        self.assertNotEqual(productos,[])
+
+    def test_regresa_ticket_formato_srt(self):
+        self.assertEqual(str(self.ticket),"Ticket: 1 - 1/1/2024 - 220")
+
+
