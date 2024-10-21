8881019be737669bb70a55a0f622c39c0a4f081c
987sebasBeller <sebastianbeller76@gmail.com>
2024-10-21 00:16:38 -0400

    Se mejoran los nombres de las pruebas unitarias de inventario

--- tests/G_inventario/test_inventario.py
+++ tests/G_inventario/test_inventario.py
@@ -18,16 +18,16 @@ class TestInventario(unittest.TestCase):
     def tearDown(self):
         self.inventario = None
 
-    def test_agregar_tc1(self):
+    def test_agregar_agrega_producto_existente(self):
         self.inventario.productos={"Aretes de plata":0}
         self.inventario.agregar_producto(self.producto)
         self.assertIn(self.producto,self.inventario.productos_catalogo)
 
-    def test_agregar_tc2(self):
+    def test_agregar_nuevo_producto(self):
         self.inventario.agregar_producto(self.producto)
         self.assertIn(self.producto, self.inventario.productos_catalogo)
 
-    def test_obtener_producto_tc1(self):
+    def test_obtener_producto_de_lista_por_nombre(self):
         self.inventario.productos_catalogo=[MagicMock(spec=Producto),MagicMock(spec=Producto)]
         self.inventario.productos_catalogo[0].get_nombre.return_value="Pulsera de oro"
         self.inventario.productos_catalogo[0].get_modelo.return_value="Modelo"
@@ -35,63 +35,63 @@ class TestInventario(unittest.TestCase):
         resp=self.inventario.obtener_producto("pulsera de plata")
         self.assertIsNotNone(resp)
 
-    def test_obtener_producto_tc2(self):
+    def test_obtener_producto_de_lista_por_modelo(self):
         self.inventario.productos_catalogo = [MagicMock(spec=Producto)]
         self.inventario.productos_catalogo[0].get_nombre.return_value = "Pulsera de oro"
         self.inventario.productos_catalogo[0].get_modelo.return_value = "Modelo"
         resp = self.inventario.obtener_producto("modelo")
         self.assertIsNotNone(resp)
 
-    def test_obtener_producto_tc3(self):
+    def test_obtener_producto_sin_coincidencias_en_identificador(self):
         self.inventario.productos_catalogo = [MagicMock(spec=Producto)]
         self.inventario.productos_catalogo[0].get_nombre.return_value = "Pulsera de oro"
         self.inventario.productos_catalogo[0].get_modelo.return_value = "Modelo"
         resp = self.inventario.obtener_producto("Ninguno")
         self.assertIsNone(resp)
 
-    def test_obtener_producto_tc4(self):
+    def test_obtener_producto_de_lista_vacia(self):
         resp = self.inventario.obtener_producto("Niguno")
         self.assertIsNone(resp)
 
-    @patch('builtins.print')  # Mockear la función print
-    def test_eliminar_producto_tc1(self, mock_print):
+    @patch('builtins.print')
+    def test_eliminar_producto_existente(self, mock_print):
         self.inventario.productos = {"mochila": 0}
         self.inventario.eliminar_producto("mochila")
         mock_print.assert_called_with("\nProducto eliminado correctamente.")
 
     @patch('builtins.print')
-    def test_eliminar_tc2(self,mock_print):
+    def test_eliminar_inexistente(self,mock_print):
         self.inventario.productos = {}
         self.inventario.eliminar_producto("X")
         mock_print.assert_called_with("\nEl producto no existe en el inventario.")
 
-    def test_buscar_tc1(self):
+    def test_buscar_existente(self):
         self.inventario.productos = {"laptop": 0}
         resp=self.inventario.buscar_producto("laptop")
         self.assertEqual(resp,"laptop: 0")
 
-    def test_buscar_tc2(self):
+    def test_buscar_inexistente(self):
         self.inventario.productos = {}
         resp=self.inventario.buscar_producto("laptop")
         self.assertEqual(resp,"\nProducto no encontrado.")
 
-    def test_actualizar_stock_tc1(self):
+    def test_actualizar_stock_producto_existente(self):
             self.inventario.productos = {"laptop": 0}
             self.inventario.actualizar_stock("laptop",10)
             self.assertEqual(self.inventario.productos,{"laptop":10})
 
     @patch('builtins.print')
-    def test_actualizar_stock_tc2(self,mock_print):
+    def test_actualizar_stock_producto_inexistente(self,mock_print):
             self.inventario.productos = {}
             self.inventario.actualizar_stock("laptop",5)
             mock_print.assert_called_with("\nEl producto no existe en el inventario.")
 
-    def test_obtener_lista_productos_tc1(self):
+    def test_obtener_lista_productos(self):
             self.assertIsNotNone(self.inventario.obtener_lista_productos())
 
-    def test_obtener_inventario_tc1(self):
+    def test_obtener_inventario_de_productos(self):
             self.assertIsNotNone(self.inventario.obtener_inventario())
 
-    def test_inicializar_inventario_tc1(self):
+    def test_inicializar_inventario(self):
             self.inventario.inicializar_inventario()
             self.assertNotEqual(self.inventario.productos_catalogo,[])
